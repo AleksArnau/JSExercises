a9f1c875a57f3000ddc47be0973be2e86deef178
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2021-11-03 03:35:43 +0100

    easy.js comments removed

--- src/easy.js
+++ src/easy.js
@@ -1,7 +1,3 @@
-// easy
-
-// find the largest number in an array, function findLargest(numbers) {} // if empty return negative infinity
-
 function findLargestNumberInArray(array) {
   let largestNumber = -Infinity;
   for (let i = 0; i < array.length; i++) {
@@ -12,32 +8,6 @@ function findLargestNumberInArray(array) {
   return largestNumber;
 }
 
-//same thing using a single line of code
-function bestPractice(array) {
-  return Math.max(...array);
-}
-
-//testCases have been moved over to ../test/easy.test.js
-//keeping this here as a comment
-// const testCasesFindLargest = [
-//   { answer: -Infinity, testCase: [] },
-//   { answer: 1, testCase: [1] },
-//   { answer: 2, testCase: [2, 2, 2, 2] },
-//   { answer: 12, testCase: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12] },
-//   { answer: 68, testCase: [1, 3, 5, 68, 0, -15] },
-//   { answer: 321, testCase: [321, 1, 2, 3] },
-// ];
-// console.log("Find largest number in array and best practice test cases:");
-// for (const { answer, testCase } of testCasesFindLargest) {
-//   const result = findLargestNumberInArray(testCase);
-//   const best = bestPractice(testCase);
-//   console.log(answer === result, answer, result);
-//   console.log(answer === best, answer, best);
-// }
-
-// i18n, function shorten(string) {}
-//empty string, one, two and three characters stay unchanged
-
 function shortenString(string) {
   if (string.length <= 3) {
     return string;
@@ -45,33 +15,6 @@ function shortenString(string) {
   return string[0] + (string.length - 2) + string[string.length - 1];
 }
 
-//testCases moved over to ../test/easy.test.js
-// const testCasesshortenString = [
-//   "true", // t2e
-//   "desktop", // d5p
-//   "function", //f6n
-//   "accessibility", // a11y
-//   "internationalization", // i18n
-//   "", // ""
-//   "a", // a
-//   "in", // in
-//   "eat", // eat
-// ];
-// console.log("String shortener test cases:");
-// for (let i = 0; i < testCasesshortenString.length; i++) {
-//   console.log(testCasesshortenString[i]);
-//   console.log(shortenString(testCasesshortenString[i]));
-// }
-
-// print multiplication tables for 10 through 10, function printTable() {}
-//tab separated numbers (use \t and \nto format)
-
-// 1 2 3 4 5 6 7 8 9 10
-// 2 4 6 8 10 ...
-// 3 6 ...
-
-//needs a wide console to properly format as
-//tab was used to keep even spaces between numbers
 function createTimesTable() {
   let tableString = "";
   for (let i = 1; i < 11; i++) {
