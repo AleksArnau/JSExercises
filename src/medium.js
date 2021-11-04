f7e61a4767f5cadccf5af2956de960a40d0546be
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2021-11-04 02:16:38 +0100

    removed comments from medium.js

--- src/medium.js
+++ src/medium.js
@@ -1,23 +1,3 @@
-//medium
-
-// determine if a word is a palindrome, function isPalindrome(string) {}
-
-// const testCasesIsPalindrome = [
-//   "true", // false sL4
-//   "tret", // false sL4
-//   "tredt", //false sL5
-//   "desktop", // false
-//   "function", //false
-//   "accessibility", //false
-//   "internationalization", // false
-//   "", // true
-//   "in", // false
-//   "lol", // true
-//   "racecar", //true
-//   "kayak", //true
-//   "madeupworddrowpuedam", //true
-// ];
-
 function isPalindrome(string) {
   for (let i = 0; i < string.length / 2; i++) {
     if (string[i] !== string[string.length - 1 - i]) {
@@ -26,18 +6,6 @@ function isPalindrome(string) {
   }
   return true;
 }
-
-// function cheapIsPalindrome(string) {
-//   return string === string.split("").reverse().join("");
-// }
-
-//uncomment to run test cases for isPalindrome()
-// for (let i = 0; i < testCasesIsPalindrome.length; i++) {
-//   console.log(cheapIsPalindrome(testCasesIsPalindrome[i]));
-// }
-
-// sum 0 to n with even/odd/all, function sumToN(n, type) {}
-//
 
 function sumToN(n, type) {
   let typeIncrement = 1;
@@ -54,39 +22,6 @@ function sumToN(n, type) {
   }
   return sum;
 }
-
-// for (let i = 0; i < testCases.length; i++) {
-//   for (let j = 0; j < testCaseTypes.length; j++) {
-//     console.log(
-//       "testcase ",
-//       testCases[i],
-//       "type",
-//       testCaseTypes[j],
-//       sumToN(testCases[i], testCaseTypes[j])
-//     );
-//   }
-// }
-
-// print pyramid, function printPyramid(height) {} middle aligned
-
-// height = 0
-// height = 1
-// *
-
-// height = 2
-//  *
-// ***
-
-// height = 3
-//   *
-//  ***
-// *****
-
-// heigth = 4
-//    * h-(i+1) + 2i+1 i=0
-//   *** h-(i+1) + 2i+1 i=1
-//  ***** h-(i+1) + 2i+1 i=2
-// *******
 
 function makePyramid(height) {
   let pyramid = "";
