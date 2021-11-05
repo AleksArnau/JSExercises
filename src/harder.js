345e68078ec0dde57a0357b3e9f4c398fbda68fe
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2021-11-05 04:16:12 +0100

    commented a possible solution to isAnagram

--- src/harder.js
+++ src/harder.js
@@ -1,16 +1,29 @@
-const { findLargestNumberInArray } = require("./easyAndMedium");
 // harder
 
 // check if anagram, function isAnagram(string1, string2) {}
+// case insensitive, space sensitive, a word cannot be it's own anagram
+// compare the two string against ea. other /w 1 object
+
+// example with string1="dog" and string2="god"
+// anagramTally takes on string1 >>> { d:1 , o:1 , g:1 }
+// for of should work for a string e.g.
+// for (const c of text) {
+//   console.log(c)
+// }
+// can check back previous exercises for how to implement xTally
+// we check it agaisnt string2 with the same dissection
+// if the tally of each letter matches we return true
+
 function isAnagram(string1, string2) {
-  if (string1 === string2) console.log("Some string");
+  if (string1 === string2) return false;
+
+  const anagramTally = {};
 }
 
 // sort a list in place, function sort(numbers) {}
 function sort(numbers) {
   let a = numbers;
-  console.log("Some string");
+  return "Some string";
 }
 
-console.log(findLargestNumberInArray([1, 2, 3, 4, 5, 6, 7, 8]));
 export { isAnagram, sort };
--- test/harder.test.js
+++ test/harder.test.js
@@ -1,20 +1,14 @@
 import { describe, expect, test } from "@jest/globals";
 import { isAnagram, sort } from "../src/harder";
-const { describe, expect, test } = require("@jest/globals");
-const { isAnagram, sort } = require("./harder");
 
-test("Empty String is not an anagram to an empty string", () => {
-  expect(isAnagram("", "")).toBe(false);
-});
-
-test("A word is not an anagram to itself", () => {
-  expect(isAnagram("pepsi", "pepsi")).toBe(false);
-});
-
-test("A different word is not an anagram", () => {
-  expect(isAnagram("pepsi", "cola")).toBe(false);
-});
-
-test("An anagram test case", () => {
-  expect(isAnagram("pepsi", "ispep")).toBe(true);
+describe.each([
+  { string1: "", string2: "", expected: false },
+  { string1: "dog", string2: "god", expected: true },
+  { string1: "Anagram", string2: "", expected: false },
+  { string1: "Anagram", string2: "margAna", expected: true },
+  { string1: "Anagram", string2: "Anagram", expected: false },
+])("", ({ string1, string2, expected }) => {
+  test(`isAnagram given ${string1} and ${string2} should return ${expected}`, () => {
+    expect(isAnagram(string1, string2)).toBe(expected);
+  });
 });
