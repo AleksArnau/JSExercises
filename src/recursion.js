5ac446614d1fc954eb122c48e047e4a2525b05a7
Aleksandar Arnautovic <aleksandar96@hotmail.fr>
2021-11-05 03:31:55 +0100

    Tests recursion (#4)
    * added code to recursion.js
    
    * tested all the imported.js functions
    
    * removed comments and test functions
    
    * comment syntax based cyberbullying

---
+++ src/recursion.js
@@ -0,0 +1,84 @@
+function iteratedFactorial(n) {
+  if (n < 0) {
+    return undefined;
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+
+  let factor = 1;
+
+  for (let i = 1; i <= n; ++i) {
+    factor *= i;
+  }
+
+  return factor;
+}
+
+function recursedFactorial(n) {
+  if (n < 0) {
+    return undefined;
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * recursedFactorial(n - 1);
+}
+
+function iteratedFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i < n; ++i) {
+    [a, b] = [b, a + b];
+  }
+
+  return b;
+}
+
+// TODO: redo this function using dynamic programming / memoization
+function recursedFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return recursedFibonacci(n - 1) + recursedFibonacci(n - 2);
+}
+
+function iteratedReverseString(string) {
+  let reversedString = "";
+
+  for (let i = string.length - 1; i >= 0; --i) {
+    reversedString += string[i];
+  }
+
+  return reversedString;
+}
+
+function recursedReverseString(string) {
+  if (string.length <= 1) {
+    return string;
+  }
+
+  return recursedReverseString(string.substring(1)) + string[0];
+}
+
+export {
+  iteratedFactorial,
+  recursedFactorial,
+  iteratedFibonacci,
+  recursedFibonacci,
+  iteratedReverseString,
+  recursedReverseString,
+};
--- test/recursion.test.js
+++ test/recursion.test.js
@@ -1,5 +1,58 @@
-describe("foo", () => {
-  test("bar", () => {
-    expect(1 + 1).toBe(2);
+import { describe, expect, test } from "@jest/globals";
+import {
+  iteratedFactorial,
+  recursedFactorial,
+  iteratedFibonacci,
+  recursedFibonacci,
+  iteratedReverseString,
+  recursedReverseString,
+} from "../src/recursion";
+
+describe.each([
+  { input: -1, expected: undefined },
+  { input: 0, expected: 1 },
+  { input: 1, expected: 1 },
+  { input: 2, expected: 2 },
+  { input: 3, expected: 6 },
+  { input: 5, expected: 120 },
+  { input: 10, expected: 3628800 },
+])("", ({ input, expected }) => {
+  test(`iteratedFactorial given ${input} should return ${expected}`, () => {
+    expect(iteratedFactorial(input)).toBe(expected);
+  });
+  test(`recursedFactorial given ${input} should return ${expected}`, () => {
+    expect(recursedFactorial(input)).toBe(expected);
+  });
+});
+
+describe.each([
+  { input: 0, expected: 0 },
+  { input: 1, expected: 1 },
+  { input: 2, expected: 1 },
+  { input: 3, expected: 2 },
+  { input: 5, expected: 5 },
+  { input: 7, expected: 13 },
+  { input: 10, expected: 55 },
+])("", ({ input, expected }) => {
+  test(`iteratedFibonacci given ${input} should return ${expected}`, () => {
+    expect(iteratedFibonacci(input)).toBe(expected);
+  });
+  test(`recursedFibonacci given ${input} should return ${expected}`, () => {
+    expect(recursedFibonacci(input)).toBe(expected);
+  });
+});
+
+describe.each([
+  { input: "", expected: "" },
+  { input: "A", expected: "A" },
+  { input: "Hello World!", expected: "!dlroW olleH" },
+  { input: "This is a string", expected: "gnirts a si sihT" },
+  { input: "kayak", expected: "kayak" },
+])("", ({ input, expected }) => {
+  test(`iteratedReverseString given ${input} should return ${expected}`, () => {
+    expect(iteratedReverseString(input)).toBe(expected);
+  });
+  test(`recursedReverseString given ${input} should return ${expected}`, () => {
+    expect(recursedReverseString(input)).toBe(expected);
   });
 });
