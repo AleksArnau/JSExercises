b3f201872efce97de083a0edf4feb5305fcf273f
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2021-11-05 03:40:07 +0100

    Added harder.js instructions and basic imports for  harder.test.js

---
+++ src/harder.js
@@ -0,0 +1,16 @@
+const { findLargestNumberInArray } = require("./easyAndMedium");
+// harder
+
+// check if anagram, function isAnagram(string1, string2) {}
+function isAnagram(string1, string2) {
+  if (string1 === string2) console.log("Some string");
+}
+
+// sort a list in place, function sort(numbers) {}
+function sort(numbers) {
+  let a = numbers;
+  console.log("Some string");
+}
+
+console.log(findLargestNumberInArray([1, 2, 3, 4, 5, 6, 7, 8]));
+export { isAnagram, sort };
--- test/harder.test.js
+++ test/harder.test.js
@@ -1,5 +1,20 @@
-describe("foo", () => {
-  test("bar", () => {
-    expect(1 + 1).toBe(2);
-  });
+import { describe, expect, test } from "@jest/globals";
+import { isAnagram, sort } from "../src/harder";
+const { describe, expect, test } = require("@jest/globals");
+const { isAnagram, sort } = require("./harder");
+
+test("Empty String is not an anagram to an empty string", () => {
+  expect(isAnagram("", "")).toBe(false);
+});
+
+test("A word is not an anagram to itself", () => {
+  expect(isAnagram("pepsi", "pepsi")).toBe(false);
+});
+
+test("A different word is not an anagram", () => {
+  expect(isAnagram("pepsi", "cola")).toBe(false);
+});
+
+test("An anagram test case", () => {
+  expect(isAnagram("pepsi", "ispep")).toBe(true);
 });
