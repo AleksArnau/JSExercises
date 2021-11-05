54032851fa007dd8610efff7b0be69419e1807de
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2021-11-05 03:20:20 +0100

    added TODO and added spacing between lines

--- src/recursion.js
+++ src/recursion.js
@@ -2,13 +2,17 @@ function iteratedFactorial(n) {
   if (n < 0) {
     return undefined;
   }
+
   if (n === 0) {
     return 1;
   }
+
   let factor = 1;
+
   for (let i = n; i > 0; --i) {
     factor *= i;
   }
+
   return factor;
 }
 
@@ -16,9 +20,11 @@ function recursedFactorial(n) {
   if (n < 0) {
     return undefined;
   }
+
   if (n === 0) {
     return 1;
   }
+
   return n * recursedFactorial(n - 1);
 }
 
@@ -26,29 +32,37 @@ function iteratedFibonacci(n) {
   if (n === 0) {
     return 0;
   }
+
   let a = 0;
   let b = 1;
+
   for (let i = 1; i < n; ++i) {
     [a, b] = [b, a + b];
   }
+
   return b;
 }
 
+//TODO redo this function using dynamic programming, memoization is also relevant
 function recursedFibonacci(n) {
   if (n === 0) {
     return 0;
   }
+
   if (n === 1) {
     return 1;
   }
+
   return recursedFibonacci(n - 1) + recursedFibonacci(n - 2);
 }
 
 function iteratedReverseString(string) {
   let reversedString = "";
+
   for (let i = string.length - 1; i >= 0; --i) {
     reversedString += string[i];
   }
+
   return reversedString;
 }
 
@@ -56,6 +70,7 @@ function recursedReverseString(string) {
   if (string.length <= 1) {
     return string;
   }
+
   return recursedReverseString(string.substring(1)) + string[0];
 }
 
